6f681664b4806323d5f08469f38227c7cde422a8
Cris <crisheight@gmail.com>
2024-06-16 18:41:12 -0500

    Created a computerchoice function to determine rock/paper/scissors at random.

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hello")
+function getComputerChoice(max) {
+    computerChoice = Math.floor(Math.random() * max);
+    if (computerChoice === 0 ) {
+        return "rock";
+    }
+
+    else if (computerChoice === 1) {
+        return "paper";
+    }
+
+    else {
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice(3));
