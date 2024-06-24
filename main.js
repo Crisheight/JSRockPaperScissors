1d9a22c0d8babb23f6960ca9f30d293432439a4e
Cris <crisheight@gmail.com>
2024-06-23 19:00:57 -0500

    Created and implemented a function for getting user choice

--- main.js
+++ main.js
@@ -14,6 +14,39 @@ function getComputerChoice(max) {
         console.log("scissors");
         return computerChoice;
     }
-}
+} // End getComputerChoice
 
 console.log(getComputerChoice(3));
+
+function getUserChoice() {
+    let userChoice = prompt("Enter rock, paper, or scissors: ");
+    userChoice = userChoice.toLowerCase();
+
+    if (userChoice === "rock") {
+        console.log("rock");
+        userChoice = 0;
+        return userChoice;
+    }
+
+    else if (userChoice === "paper") {
+        console.log("paper");
+        userChoice = 1;
+        return userChoice;
+    }
+
+    else if (userChoice === "scissors") {
+        console.log("scissors");
+        userChoice = 2;
+        return userChoice;
+    }
+
+    else {
+        console.log("Invalid input");
+        return "Invalid input";
+    }
+} // End getUserChoice
+
+console.log(getUserChoice());
+
+
+
