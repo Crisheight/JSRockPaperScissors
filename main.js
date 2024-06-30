a786de12bdba16a306d58a04dcc861d340650efd
Cris <crisheight@gmail.com>
2024-06-30 15:00:21 -0500

    Create a runGame method that calls all previous created methods n times. Then, call runGame(n) for desired result.

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ function getComputerChoice(max) {
     }
 } // End getComputerChoice
 
-console.log(getComputerChoice(3));
-
 function getUserChoice() {
     let userChoice = prompt("Enter rock, paper, or scissors: ");
     userChoice = userChoice.toLowerCase();
@@ -46,7 +44,54 @@ function getUserChoice() {
     }
 } // End getUserChoice
 
-console.log(getUserChoice());
+function keepScore(computerChoice, userChoice) {
+    let bothScores = [0,0];
+    console.log("User choice: " + userChoice);
+    console.log("Computer choice: " + computerChoice)
+
+    if (userChoice === computerChoice) {
+        console.log("It's a tie!");
+    }
+
+    else if (userChoice === 0 && computerChoice === 2) {
+        console.log("You win!");
+        bothScores[0] = bothScores[0] + 1;
+    }
+
+    else if (userChoice === 1 && computerChoice === 0) {
+        console.log("You win!");
+        bothScores[0] = bothScores[0] + 1;
+    }
+
+    else if (userChoice === 2 && computerChoice === 1) {
+        console.log("You win!");
+        bothScores[0] = bothScores[0] + 1;
+    }
+
+    else {
+        console.log("Computer wins!");
+        bothScores[1] = bothScores[1] + 1;
+    }
+
+    console.log("User score: " + bothScores[0]);
+    console.log("Computer score: " + bothScores[1]);
+    return bothScores;
+} // End keepScore
+
+function runGame(n) {
+    let i = 0;
+    let scoreTracker = [0,0];
+    while (i < n) {
+        let computerChoice = getComputerChoice(3);
+        let userChoice = getUserChoice();
+        scoreTracker = scoreTracker + keepScore(computerChoice, userChoice);
+        i++;
+    }
+    console.log("Final score: " + scoreTracker);
+} // End runGame
+
+runGame(5);
+
 
 
 
